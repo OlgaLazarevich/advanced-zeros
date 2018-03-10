3ec80feba3595d26a956feaf20db6432c807b0cd
helga <helga>
2018-03-10 12:29:35 +0300

    advanced-zeros: the first solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,53 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  
+  
+
+    var zerosCount = 0;
+
+    var num = base;
+
+    var mainNum = 0;
+
+
+
+
+for (var i = 0; i < num ; i++) {
+    
+    if (num == 10) {
+        mainNum = 5;
+    }
+    
+    else if (i > 0 && num % i == 0) {
+    
+    var a = Math.floor(num / i),
+        b = Math.floor(num / 7);
+    
+    
+    if (a * i == num && i > b) {
+        mainNum = i;
+       break;
+      
+    }
+    }
+    
+}
+
+
+if (mainNum > 5) {
+    mainNum = mainNum;
+} else {mainNum = 5;}
+
+
+
+    while (number > 0) {
+        number = Math.floor(number/mainNum);
+        
+    zerosCount += number;
+        
+
+    }
+    
+    return zerosCount;
+
+
 }
